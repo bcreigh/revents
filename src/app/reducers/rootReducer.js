b14c1b6f33136506a6e38953bc611c0a979c55a5
Bob Creigh <bcreigh@bobcreigh.com>
2018-07-03 17:06:06 -0400

    End of section 9

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import { reducer as FormReducer } from "redux-form";
 import eventReducer from "../../features/event/eventReducer";
 import testReducer from "../../features/testArea/testReducer";
 
 const rootReducer = combineReducers({
+  form: FormReducer,
   events: eventReducer,
   test: testReducer
 });
